ebf6fd02be5f9b47990e449ec7ca12e9707f3429
Alberto <albertobaccega02@gmail.com>
2019-03-27 13:45:27 +0100

    Lavoro completato

--- Baccega_Alberto_RIFLESSI/Baccega_Alberto_RIFLESSI.ino
+++ Baccega_Alberto_RIFLESSI/Baccega_Alberto_RIFLESSI.ino
@@ -2,14 +2,19 @@
 
 int PulsanteAccensione;   //Pulsante per accendere il PROGRAMMA
 int LedRandom;            //LED che si accenderà in modo RANDOMICO
-int PulsanteTempoLed;     //Pulsante per spegnere il LED
+int PulsanteTempoLed;     //Pulsante per spegnere il LED e poter calcolare i MILLISECONDI INIZIALI
 int TempoLed;             //Quanto TEMPO ci mette a premere il PULSANTE del LED RANDOM
-int Buzzer;               //BUZZER che si accenderà in modo RANDOM
-int PulsanteBuzzer;       //Pulsante per spegnere il BUZZER
+int Buzzer;               //BUZZER che si accenderà in modo RANDOMDOMICO
+int PulsanteBuzzer;       //Pulsante per spegnere il BUZZER e poter calcolare i MILLISECONDI FINALI
 int TempoBuzzer;          //Quanto TEMPO ci mette a premere il PULSANTE del BUZZER RANDOM
 int LedVerde;             //LED VERDE se il tempo è minore di 1000 ms
 int LedRosso;             //LED ROSSO se il tempo è maggiore dei 1000 ms
-
+int Appoggio;
+int Appoggio2;
+int Appoggio3;
+int Appoggio4;
+int Appoggio5;
+int Appoggio6;
 
 void setup() {
   //assegno le VARIABILI citate sopra
@@ -21,41 +26,50 @@ void setup() {
   LedVerde = 8;
   LedRosso = 9;
   Serial.begin(9600);
-  pinMode(PulsanteAccensione, INPUT);
+  pinMode(PulsanteAccensione, INPUT);  //Inserisco INPUT per tutti i PULSANTI
   pinMode(LedRandom, OUTPUT);
-  pinMode(PulsanteTempoLed, INPUT);
+  pinMode(PulsanteTempoLed, INPUT);    //Inserisco INPUT per tutti i PULSANTI
   pinMode(Buzzer, OUTPUT);
-  pinMode(PulsanteBuzzer, INPUT);
+  pinMode(PulsanteBuzzer, INPUT);      //Inserisco INPUT per tutti i PULSANTI
   pinMode(LedVerde, OUTPUT);
   pinMode(LedRosso, OUTPUT);
 }
 
 void loop() {
-  TestRiflessi("Premi il BOTTONE 1 per INIZIARE LA PROVA",PulsanteAccensione,LedRandom);
-
+  TestRiflessi("Premi il PULSANTE DI ACCENSIONE per iniziare il TEST",PulsanteAccensione,LedRandom,Appoggio,Appoggio2,Appoggio3,Appoggio4,Appoggio5,Appoggio6);
 }
 
-void TestRiflessi(String inizio,int domanda,int risposta)
+void TestRiflessi(String inizio,int domanda,int risposta, int a, int b, int c, int d, int e,int f)
 {
   Serial.println(inizio);
   while(digitalRead(PulsanteAccensione) == LOW) {};
   while(digitalRead(PulsanteAccensione) == HIGH)
   {
+    d = millis();
     digitalWrite(LedRandom,HIGH);
-    delay(random(1000, 10000);
+    delay(random(1000, 10000));
     a = millis();
+    e = a - d;
     while(digitalRead(PulsanteTempoLed) == HIGH)
     {
+      f = millis();
       digitalWrite(Buzzer,HIGH);
       delay(random(1000,10000));
       b = millis();
+      c = b - f;
       while(digitalRead(PulsanteBuzzer) == HIGH)
       {
-        Serial.print(b - a);
-      }
-      
+        Serial.println("LED"+String(e) +"BUZZER"+String(c));
+        if ( e > 1000 && c > 1000 )
+        {
+          digitalWrite(LedRosso, HIGH);
+        }
+        else
+        {
+          digitalWrite(LedVerde, HIGH);
+        }
+      }    
     }
   }
   
 }
-
